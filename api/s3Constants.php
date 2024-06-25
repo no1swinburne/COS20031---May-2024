a4c41d1aad59fa6f35ac050caed31e5f791f7428
nisueya <110908339+defurl@users.noreply.github.com>
2024-06-26 01:34:24 +0700

    Update s3Constants.php

--- api/s3Constants.php
+++ api/s3Constants.php
@@ -1,8 +1,1 @@
-<?php
-// S3 bucket connection
-define('S3_REGION', 'us-east-1');
-define('S3_VERSION', 'latest');
-define('IAM_ACCESS_KEY_ID', 'AKIA2UC3CIWVBXIQXJN5');
-define('IAM_SECRET_ACCESS_KEY_ID', 'viZINI5tFjcBE1FPXe8SFy3+IVbDpnBOOlE4dPMC');
-define('S3_BUCKET', 'cos20031-chelseano1-test');
-?>
+
